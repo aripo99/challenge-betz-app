2cbec9e13baf743e2fbeb79ded4332f4d3f6baca
aripo99 <a_polakof@umail.ucsb.edu>
2024-05-24 09:40:39 -0700

    Add cards

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -1,15 +1,37 @@
-import { Button, ButtonText, Text } from "@gluestack-ui/themed";
+import { Button, ButtonText, Card, Heading, Text } from "@gluestack-ui/themed";
+
+const challenges = [
+    {
+        name: 'Go to the gym',
+        description: 'It only counts if it\'s more than 30 minutes'
+    },
+    {
+        name: 'Read',
+        description: 'Read for at least 30 minutes'
+    }
+]
+
 export default function Home() {
     return (
         <>
-            <Text style={{ fontSize: 24, textAlign: 'center', margin: 10 }}>
+            <Text m="$4" style={{ fontSize: 24, textAlign: 'center', color: '#fff' }}>
                 Your Challenges
             </Text>
+
+            {challenges.map(challenge => (
+                <Card size="md" variant="outline" m="$3">
+                    <Heading mb="$1" size="md">
+                        {challenge.name}
+                    </Heading>
+                    <Text size="sm">{challenge.description}</Text>
+                </Card>
+            ))}
+
             <Button
                 size="md"
                 variant="outline"
                 action="primary"
-                style={{ marginBottom: 10 }}
+                m="$3"
             >
                 <ButtonText>Create</ButtonText>
             </Button>
@@ -17,6 +39,7 @@ export default function Home() {
                 size="md"
                 variant="outline"
                 action="primary"
+                mx="$3"
             >
                 <ButtonText>Join</ButtonText>
             </Button>
