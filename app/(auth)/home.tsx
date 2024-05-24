006793c8055e961eac5b09f16dfb2cb3a5ffece9
aripo99 <a_polakof@umail.ucsb.edu>
2024-05-24 13:51:10 -0700

    Add list challenges

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -1,23 +1,64 @@
 import { Button, ButtonText, Card, Heading, Text } from "@gluestack-ui/themed";
 import { Link } from "expo-router";
 import { Pressable } from "react-native";
+import { useEffect, useState } from "react";
+import { supabase } from '@/utils/supabase';
 
-const challenges = [
-    {
-        id: 1,
-        name: 'Go to the gym',
-        description: 'It only counts if it\'s more than 30 minutes',
-        isCompleted: true
-    },
-    {
-        id: 2,
-        name: 'Read',
-        description: 'Read for at least 30 minutes',
-        isCompleted: false
-    }
-]
+interface Challenge {
+    id: number;
+    name: string;
+    description: string;
+    isCompleted: boolean;
+}
 
 export default function Home() {
+    const [challenge, setChallenge] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [challenges, setChallenges] = useState([]);
+
+    // Load todos on start
+    useEffect(() => {
+        loadChallenges();
+    }, []);
+
+    async function loadChallenges() {
+        const {
+            data: challenges,
+            error,
+        } = await supabase.from('challenges').select('*');
+
+        if (error) {
+            console.error('Error loading challenges:', error.message);
+        } else {
+            console.log(challenges);
+            setChallenges(challenges);
+        }
+    }
+
+    const addChallenge = async () => {
+        const {
+            data: { user: User },
+        } = await supabase.auth.getUser();
+
+        console.log(User);
+        const newChallenge = {
+            created_by: User?.id,
+            challenge_name: 'temp' || challenge.name,
+            challenge_description: 'temp' || challenge.description,
+            challenge_start_date: new Date(),
+            challenge_end_date: new Date(),
+            password: '123',
+        };
+
+        const { data: challenge, error } = await supabase.from('challenges').insert(newChallenge).single();
+        if (error) {
+            console.log('Error inserting challenge:', error.message);
+        }
+        else {
+            setChallenges([...challenges, challenge]);
+        }
+    };
+
     return (
         <>
             <Text m="$4" style={{ fontSize: 24, textAlign: 'center', color: '#fff' }}>
@@ -25,13 +66,13 @@ export default function Home() {
             </Text>
 
             {challenges.map(challenge => (
-                <Link key={challenge.id} href={`/challenge/${challenge.id}`} asChild>
+                <Link key={challenge.challenge_id} href={`/challenge/${challenge.id}`} asChild>
                     <Pressable>
                         <Card size="md" variant="outline" m="$3">
                             <Heading mb="$1" size="md">
-                                {challenge.name}
+                                {challenge.challenge_name}
                             </Heading>
-                            <Text size="sm">{challenge.description}</Text>
+                            <Text size="sm">{challenge.challenge_description}</Text>
                         </Card>
                     </Pressable>
                 </Link>
@@ -42,6 +83,7 @@ export default function Home() {
                 variant="outline"
                 action="primary"
                 m="$3"
+                onPress={addChallenge}
             >
                 <ButtonText>Create</ButtonText>
             </Button>
