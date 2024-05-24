d3a0058dcb70efd338329dde724af4316cb8c4c4
aripo99 <a_polakof@umail.ucsb.edu>
2024-05-24 09:30:20 -0700

    Add buttons

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -1,8 +1,22 @@
-import { Text } from "@gluestack-ui/themed";
+import { Button, ButtonText, Text } from "@gluestack-ui/themed";
 export default function Home() {
     return (
         <>
             <Text>Home</Text>
+            <Button
+                size="md"
+                variant="outline"
+                action="primary"
+            >
+                <ButtonText>Create</ButtonText>
+            </Button>
+            <Button
+                size="md"
+                variant="outline"
+                action="primary"
+            >
+                <ButtonText>Join</ButtonText>
+            </Button>
         </>
     )
 }
