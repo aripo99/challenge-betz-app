116314fc3ebbffd819a7273a68b56f2d7b425ae2
aripo99 <a_polakof@umail.ucsb.edu>
2024-05-24 09:34:33 -0700

    Add title

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -2,11 +2,14 @@ import { Button, ButtonText, Text } from "@gluestack-ui/themed";
 export default function Home() {
     return (
         <>
-            <Text>Home</Text>
+            <Text style={{ fontSize: 24, textAlign: 'center', margin: 10 }}>
+                Your Challenges
+            </Text>
             <Button
                 size="md"
                 variant="outline"
                 action="primary"
+                style={{ marginBottom: 10 }}
             >
                 <ButtonText>Create</ButtonText>
             </Button>
