37cb6aa28d01d7b69b638c5ea7bc3426cf3046e2
aripo99 <a_polakof@umail.ucsb.edu>
2024-05-25 11:38:09 -0700

    Add logout button

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,9 +1,21 @@
 import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { supabase } from '@/utils/supabase';
+import { Pressable } from 'react-native';
 
 export default function Layout() {
+    const onLogout = async () => {
+        await supabase.auth.signOut();
+    }
     return (
         <Stack>
-            <Stack.Screen name="home" options={{ title: 'Challenge Betz' }} />
+            <Stack.Screen name="home" options={{
+                title: 'Challenge Betz', headerRight: () => (
+                    <Pressable onPress={onLogout}>
+                        <Ionicons name="log-out-outline" size={24} color="#fff" />
+                    </Pressable>
+                )
+            }} />
             <Stack.Screen name="challenge/[id]" options={{ title: 'Challenge Betz', headerBackTitle: 'Home' }} />
         </Stack>
     )
