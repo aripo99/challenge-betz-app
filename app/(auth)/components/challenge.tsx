101cfa86beee032a8db89cec36072d1c27311466
aripo99 <a_polakof@umail.ucsb.edu>
2024-06-05 21:34:05 -0700

    Add progress to card

--- app/(auth)/components/challenge.tsx
+++ app/(auth)/components/challenge.tsx
@@ -1,9 +1,9 @@
-import { Card, Heading, Text, Pressable } from '@gluestack-ui/themed'
-import { Challenge } from '../home'
+import { Card, Heading, Text, Pressable, HStack } from '@gluestack-ui/themed'
 import { Link } from 'expo-router'
 import AppleStyleSwipeableRow from './swipeableRow'
 import { supabase } from '@/utils/supabase'
 import { useState } from 'react'
+import { StyleSheet } from 'react-native';
 
 function convertUTCToPSTDateString(utcDateString: string): string {
     const date = new Date(utcDateString);
@@ -17,6 +17,7 @@ function convertUTCToPSTDateString(utcDateString: string): string {
 
 export function ChallengeCard({ challenge }: { challenge: any }) {
     const [userChallenge, setUserChallenge] = useState<any>(challenge);
+    console.log('userChallenge', userChallenge);
     const handleComplete = async () => {
         const { data: { user: User } } = await supabase.auth.getUser()
         const { data: challengeData, error: challengeError } = await supabase.from("user_challenges").select("*").eq("user_id", User?.id || "").eq("challenge_id", challenge.challenge_id).single();
@@ -48,14 +49,38 @@ export function ChallengeCard({ challenge }: { challenge: any }) {
         >
             <Link key={challenge.challenge_id} href={`/challenge/${challenge.challenge_id}?challengeName=${challenge.challenge_name}&challengeDescription=${challenge.challenge_description}`} asChild>
                 <Pressable>
-                    <Card size="md" variant="outline" m="$3" style={userChallenge?.isComplete ? { borderColor: '#004d00' } : {}}>
-                        <Heading mb="$1" size="md" color="white">
+                    <Card size="md" variant="outline" style={[styles.card, userChallenge.isComplete && styles.completedCard]}>
+                        <Heading mb="$1" size="md" style={styles.heading}>
                             {challenge.challenge_name}
                         </Heading>
-                        <Text size="sm" color="gray">{challenge.challenge_description}</Text>
+                        <Text size="sm" style={styles.description}>
+                            {challenge.challenge_description}
+                        </Text>
+                        <HStack mr="$2" mt="$2">
+                            <Text size="sm" mr="$2" style={styles.infoText}>Progress: {userChallenge.progress || userChallenge.user_challenges[0].progress}</Text>
+                        </HStack>
                     </Card>
                 </Pressable>
             </Link>
-        </AppleStyleSwipeableRow>
+        </AppleStyleSwipeableRow >
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        margin: 8,
+        borderColor: '#ccc',
+    },
+    completedCard: {
+        borderColor: '#004d00',
+    },
+    heading: {
+        color: 'white',
+    },
+    description: {
+        color: 'gray',
+    },
+    infoText: {
+        color: 'gray',
+    },
+});
