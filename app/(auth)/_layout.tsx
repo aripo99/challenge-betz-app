e161ca52c5122105aa35203ad8586b7b2157b050
aripo99 <a_polakof@umail.ucsb.edu>
2024-08-11 13:14:26 -0700

    Add profile page

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,11 +3,14 @@ import { Ionicons } from '@expo/vector-icons';
 import { supabase } from '@/utils/supabase';
 import { Pressable } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Layout() {
     const onLogout = async () => {
         await supabase.auth.signOut();
     }
+    const navigation = useNavigation();
+
     return (
         <GestureHandlerRootView>
             <Stack>
@@ -16,8 +19,13 @@ export default function Layout() {
                         <Pressable onPress={onLogout}>
                             <Ionicons name="log-out-outline" size={24} color="#fff" />
                         </Pressable>
+                    ), headerLeft: () => (
+                        <Pressable onPress={() => navigation.navigate('profile')}>
+                            <Ionicons name="person-circle-outline" size={24} color="#fff" />
+                        </Pressable>
                     )
                 }} />
+                <Stack.Screen name="profile" options={{ title: 'Profile', headerBackTitle: 'Home' }} />
                 <Stack.Screen name="challenge/[id]" options={{ title: 'Challenge Betz', headerBackTitle: 'Home' }} />
             </Stack>
         </GestureHandlerRootView>
---
+++ app/(auth)/profile.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+export default function Profile() {
+    return (
+        <Text style={{ fontSize: 24, textAlign: 'center', color: '#fff' }}> Profile </Text>
+    )
+}
